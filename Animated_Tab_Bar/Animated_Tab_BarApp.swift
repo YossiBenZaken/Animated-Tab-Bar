bd3aeea4992361636a3e0edaf9959ef4c4f549ba
YossiBenZaken <yossibz@gmail.com>
2022-08-01 21:53:19 +0300

    Initial Commit

---
+++ Animated_Tab_Bar/Animated_Tab_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_Tab_BarApp.swift
+//  Animated_Tab_Bar
+//
+//  Created by Yosef Ben Zaken on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Animated_Tab_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
